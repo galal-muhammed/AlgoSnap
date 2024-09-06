d95a3c6d89a77ddd33f5c1c3786acc49016c5a63
Galal Mohamed <galalmohamed25325@gmail.com>
2024-09-06 12:22:38 +0300

    changing name

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, createHashRouter, HashRouter, Route, RouterProvider, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Search from './components/Search/Search';
 import BinaryTree from './components/BinaryTree/BinaryTree';
 import Path from './components/Path/Path';
 
 function App() {
-  let routers = createHashRouter([
-    {
-      path: '/', children: [
-        { index: true, element: <Home /> },
-        { path: 'search', element: < Search /> },
-        { path: 'binarytree', element: < BinaryTree /> },
-        { path: 'shortestpath', element: < Path /> }
-      ]
-    }
-  ])
+
   return (
-    <RouterProvider router={routers} />
+    <BrowserRouter>
+      <div className='bg-algo h-fit'>
+        <div className="md:mx-16">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/search' element={<Search />} />
+            <Route path='/BT' element={<BinaryTree />} />
+            <Route path='/SP' element={<Path />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
