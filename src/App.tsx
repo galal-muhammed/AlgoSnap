91e2213e547a0107205c2a7813bb3f5adab89ce6
Galal Mohamed <galalmohamed25325@gmail.com>
2024-09-06 12:41:39 +0300

    pathing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, createHashRouter, Route, RouterProvider, Routes } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Search from './components/Search/Search';
 import BinaryTree from './components/BinaryTree/BinaryTree';
 import Path from './components/Path/Path';
-import path from 'path';
 
 function App() {
-  let routers = createHashRouter([
+  let routes = createBrowserRouter([
     {
       path: '/', children: [
-        { index: true, element: < Home /> },
-        { path: 'search', element: <Search /> },
-        { path: 'binarytree', element: <BinaryTree /> },
-        { path: 'path', element: <Path /> }
+        { path: "*", element: <Home /> },
+        { path: "BT", element: <BinaryTree /> },
+        { path: "search", element: <Search /> },
+        { path: "SP", element: <Path /> }
       ]
     }
   ])
 
   return (
-    // <BrowserRouter>
-    //   <div className='bg-algo h-fit'>
-    //     <div className="md:mx-16">
-    //       <Routes>
-    //         <Route path='/' element={<Home />} />
-    //         <Route path='/search' element={<Search />} />
-    //         <Route path='/BT' element={<BinaryTree />} />
-    //         <Route path='/SP' element={<Path />} />
-    //       </Routes>
-    //     </div>
-    //   </div>
-    // </BrowserRouter>
-    <RouterProvider router={routers} />
+    <BrowserRouter>
+      <div className='bg-algo h-fit'>
+        <div className="md:mx-16">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/search' element={<Search />} />
+            <Route path='/BT' element={<BinaryTree />} />
+            <Route path='/SP' element={<Path />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
